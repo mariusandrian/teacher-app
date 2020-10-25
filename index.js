501507cd663121c53a67c1607824cca5526ea5cb
Marius Andrian <marius.atj@gmail.com>
2020-10-25 23:54:35 +0800

    able to send correct responses

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
+const util = require('util');
+const url = require('url');
 
 // Create connection
 const db = mysql.createConnection({
@@ -20,39 +22,115 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: false}));
+const query = util.promisify(db.query).bind(db);
 
+// Post students
+app.post('/api/register', async (req, res) => {
+    try {
+        let students = req.body.students;
+        let studentValues = students.map((student, index) => {
+            return [student];
+        });
+        let sql = "INSERT IGNORE INTO student (email) VALUES ?";
+        const studentInsert = await query(sql, [studentValues]);
 
-// Create DB
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE teacher_app';
-    db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('database created');
-    })
+        let teacherId;
+        let studentId = [];
+        let getTeacherId = "SELECT id FROM teacher WHERE email = ?";
+        let getStudentId = "SELECT id FROM student WHERE email in (?) ORDER BY id";
+        
+        const getTeacherResult = await query(getTeacherId, req.body.teacher);
+        teacherId = getTeacherResult[0].id;
+
+        const getStudentResult = await query(getStudentId, [req.body.students]);
+        getStudentResult.forEach((row, index) => {
+            studentId.push([teacherId, row.id]);
+        })
+
+        let updateBridgeTable = `INSERT IGNORE INTO teacher_has_student (teacher_id, student_id) VALUES ?`;
+        const updateBridgeTableResults = await query(updateBridgeTable, [studentId]);
+
+        res.status(204).send();
+    } catch (err) {
+        console.log(err);
+    }    
 })
 
-// Create table
-app.get('/createtable', (req, res) => {
-    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
-    db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Posts table created');
-    })
+// Get common students
+app.get('/api/commonstudents', async (req, res) => {
+    try {
+        let payload = [];
+        // Parse incoming URL
+        let teacherQuery = req.query.teacher;
+        if (typeof teacherQuery === 'string') {
+            teacherQuery = [teacherQuery];
+        }
+
+        let count = teacherQuery.length;
+
+        const sql = `select s.email FROM student as s INNER JOIN teacher_has_student as ts ON s.id = ts.student_id INNER JOIN teacher as t ON ts.teacher_id = t.id WHERE t.email IN (?) GROUP BY s.email HAVING COUNT(t.email) = ?;`
+
+        const students = await query(sql, [teacherQuery, count]);
+        console.log(students);
+        students.forEach((row) => {
+            payload.push(row.email);
+        })
+
+        res.send({students: payload});
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// Suspend student
+app.post('/api/suspend', async(req, res) => {
+    try {
+        const student = req.body.student;
+        let sql = `UPDATE student SET student.suspend = "Y" WHERE student.email = ?`
+        const response = await query(sql, student);
+        console.log(response);
+        res.status(204).send();
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// Notification
+app.post('/api/retrievefornotifications', async(req, res) => {
+    try {
+        let teacher = req.body.teacher;
+        let emailPattern = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/g;
+        let mentions = req.body.notification.match(emailPattern);
+
+        let recipients = [];
+        let searchByTeacher = `select DISTINCT student.email FROM student INNER JOIN teacher_has_student ON student.id = teacher_has_student.student_id INNER JOIN teacher ON teacher_has_student.teacher_id = teacher_id WHERE teacher.email in (?) AND student.suspend = 'N';`
+
+        let checkIfSuspended = `select student.email FROM student WHERE student.email in (?) AND student.suspend = 'N'`
+
+        if (!mentions) {
+            let results = await query(searchByTeacher, teacher);
+            results.forEach((row) => {
+                recipients.push(row.email);
+            })
+        } else {
+            let results = await query(searchByTeacher, teacher);
+            results.forEach((row) => {
+                if (!mentions.includes(row.email)) {
+                    mentions.push(row.email);
+                }
+            });
+            let resultsWithoutSuspend = await query(checkIfSuspended, [mentions]);
+            results.forEach((row) => {
+                recipients.push(row.email);
+            })
+        }
+        res.status(200).json({recipients: recipients});
+    } catch (err) {
+        console.log(err);
+    }
 })
 
-// Insert post 1
-app.get('/addpost1', (req, res) => {
-    let post = {title: 'Post one', body: 'This is post number one'};
-    let sql = 'INSERT INTO posts SET ?';
-    let query = db.query(sql, post, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Post 1 created');
-    });
-})
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 })
--- teacher_app.sql
+++ teacher_app.sql
@@ -1,1 +1,81 @@
-CREATE
+USE teacher_app;
+
+CREATE TABLE student (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL,
+suspend CHAR(1) DEFAULT 'N',
+PRIMARY KEY (id)
+);
+
+CREATE TABLE teacher (
+id INT NOT NULL AUTO_INCREMENT,
+email VARCHAR(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE teacher_has_student (
+teacher_id INT,
+student_id INT,
+FOREIGN KEY (teacher_id) REFERENCES teacher(id),
+FOREIGN KEY (student_id) REFERENCES student(id),
+UNIQUE (teacher_id, student_id)
+);
+
+ALTER TABLE student
+ALTER suspend SET DEFAULT 'N';
+
+UPDATE student
+SET suspend = 'N'
+WHERE id = 120;
+
+INSERT INTO teacher_app.teacher (email)
+VALUES ('teacherjoe@gmail.com');
+
+INSERT INTO teacher_app.student (email)
+VALUES ('commonstudent2@gmail.com');
+
+INSERT INTO teacher_app.teacher_has_student (teacher_id, student_id)
+VALUES (1, 120);
+
+SELECT * from teacher_app.teacher;
+
+SELECT * from teacher_app.student;
+
+SELECT * from teacher_app.teacher_has_student;
+
+
+
+ALTER TABLE teacher_app.teacher
+ADD UNIQUE(email);
+
+SELECT teacher.id, teacher.email, student.id, student.email
+FROM teacher
+INNER JOIN teacher_has_student ON teacher.id = teacher_has_student.teacher_id
+INNER JOIN student ON student.id = teacher_has_student.student_id = student.id;
+
+select s.email, ts.student_id, ts.teacher_id, t.email 
+FROM student as s
+INNER JOIN teacher_has_student as ts ON s.id = ts.student_id
+INNER JOIN teacher as t ON ts.teacher_id = teacher_id;
+
+select ts.student_id, ts.teacher_id, t.email 
+FROM teacher_has_student as ts
+INNER JOIN teacher as t ON ts.teacher_id = t.teacher_id;
+
+WHERE teacher.email IN ('teacherken@gmail.com', 'teacherjoe@gmail.com');
+
+select DISTINCT student.email
+FROM student
+INNER JOIN teacher_has_student ON student.id = teacher_has_student.student_id
+INNER JOIN teacher ON teacher_has_student.teacher_id = teacher_id
+WHERE teacher.email = 'teacherken@gmail.com' AND student.suspend = 'N';
+
+select student.email 
+FROM student 
+WHERE student.email IN ('studentjon@gmail.com', 'studenthon@gmail.com' ) 
+AND student.suspend = 'N';
+
+ALTER TABLE teacher_app.student
+ADD UNIQUE(email);
+
+DELETE FROM teacher_app.teacher_has_student WHERE teacher_id = 1;
